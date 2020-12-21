2b3257cba51f2392439ab296b69ee9d56648d7a4
shunf4 <shun1048576@gmail.com>
2020-12-22 03:45:40 +0800

    update

--- app/src/main/java/com/shunf4/setbluetoothalwaysvisible/MainActivity.java
+++ app/src/main/java/com/shunf4/setbluetoothalwaysvisible/MainActivity.java
@@ -1,9 +1,11 @@
 package com.shunf4.setbluetoothalwaysvisible;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -13,10 +15,18 @@ import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_DISCOVERABLE_BT = 10001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onRefreshStatusClicked(null);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         onRefreshStatusClicked(null);
     }
 
@@ -57,12 +67,18 @@ public class MainActivity extends AppCompatActivity {
             setDiscoverableTimeout.setAccessible(true);
             Method setScanMode = BluetoothAdapter.class.getMethod("setScanMode", int.class, int.class);
             setScanMode.setAccessible(true);
-            setDiscoverableTimeout.invoke(ba, 0);
-            setScanMode.invoke(ba, BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE, 0);
+            setDiscoverableTimeout.invoke(ba, 36000);
+            setScanMode.invoke(ba, BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE, 36000);
         } catch (Exception e) {
             e.printStackTrace();
             Toast.makeText(this, "Error: " + e.toString(), Toast.LENGTH_LONG).show();
         }
         onRefreshStatusClicked(null);
+    }
+
+    public void onSetAlwaysVisibleNormalWayClicked(View view) {
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 3600);
+        startActivityForResult(intent, REQUEST_DISCOVERABLE_BT);
     }
 }
